c4abb78f7f025fc5016e7339b92649ae750f4b4d
TuanPham303 <phamcongtuan303@gmail.com>
2017-09-06 13:01:47 -0700

    refactoring

--- week1day3/letterPos.js
+++ week1day3/letterPos.js
@@ -1,18 +1,36 @@
-function countLetters(string){
+/*function countLetters(string){
   var output = {};
   var strArray = string.split('');
   for (var char in strArray){
-    if(/\w/.test(strArray[char]) && output[strArray[char]] !== undefined){
+    if (/\w/.test(strArray[char]) && output[strArray[char]] !== undefined){
       output[strArray[char]].push((strArray.indexOf(strArray[char])));
       strArray[char] = ' ';
     }
-    if (/\w/.test(strArray[char]) && output[strArray[char]] === undefined) {
+    if (/\w/.test(strArray[char]) && output[strArray[char]] === undefined){
       output[strArray[char]] = [];
       output[strArray[char]].push((strArray.indexOf(strArray[char])));
       strArray[char] = ' ';
     } 
   }
   return output;
+}*/
+
+// REFACTORING
+
+function countLetters(string){
+  var output = {};
+  for (var i = 0; i < string.length; i++){
+    var char = string[i];
+    if(/\w/.test(char)){
+      if(!output[char]){
+        output[char] = [];
+      }
+      output[char].push(i);
+    } 
+  }
+  return output;
 }
+
+
 console.log(countLetters("lighthouse in the house"));
 
