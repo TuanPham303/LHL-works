27c3833e5deefa42cfcf137879df93d997b40672
TuanPham303 <phamcongtuan303@gmail.com>
2017-09-06 15:20:51 -0700

    refactoring

--- week1day3/sale.js
+++ week1day3/sale.js
@@ -23,29 +23,27 @@ var companySalesData = [
 ];
 
 function totalSale(saleArray){
-  var sum = 0;
-  for (var i = 0; i < saleArray.length; i++){
-    sum += saleArray[i];
-  }
-  return sum;
+  return saleArray.reduce(function(a, c){
+    return a + c;
+  });
 }
 
 function calculateSalesTax(salesData, taxsRate){
   var report = {};
   for(var i = 0; i < salesData.length; i++){
-    salesData[i].taxs = totalSale(salesData[i].sales) * taxsRate[salesData[i].province];
-    if(!report[salesData[i].name]){
+    salesData[i].taxes = totalSale(salesData[i].sales) * taxsRate[salesData[i].province];
+    if(!report[salesData[i].name]){ 
       report[salesData[i].name] = {};
       report[salesData[i].name].totalSales = 0;
-      report[salesData[i].name].totalTaxs = 0;
+      report[salesData[i].name].totalTaxes = 0;
     }
     
     report[salesData[i].name].totalSales += totalSale(salesData[i].sales);
-    report[salesData[i].name].totalTaxs += salesData[i].taxs;
+    report[salesData[i].name].totalTaxes += salesData[i].taxes;
   }
-  console.log(report);
+  return report;
 }
 
-calculateSalesTax(companySalesData, salesTaxRates);
+console.log(calculateSalesTax(companySalesData, salesTaxRates));
 
 
